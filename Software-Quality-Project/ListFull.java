91d67eaa83a7106b908af79a7423fddb433db36a
mehak-12 <33705382+mehak-12@users.noreply.github.com>
2017-11-15 18:49:19 -0600

    Sample 1

--- Software-Quality-Project/ListFull.java
+++ Software-Quality-Project/ListFull.java
@@ -1,36 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package listfull;
-
-/**
- *
- * @author Trupti
- */
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * It creates a list to store String objects and then adds strings in an infinite loop and generates OutOfMemoryError.
+ *
+ */
 
 public class ListFull {
 
 	public static void main(String[] args) {
 
 		List<String> strList = new ArrayList<String>();
-		
+
 		int counter = 0;
 		boolean flag = true;
-		while(flag) {
-			
-			String value = "This is a leaky String "+counter++;
-			
+		while (flag) {
+
+			String value = "This is a leaky String " + counter++;
+
 			strList.add(value);
-			
+
 		}
-		
-		
+
 		System.out.println(strList.size());
-		
 
 	}
 
